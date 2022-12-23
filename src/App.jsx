55741ca00d9515280cd85bb9b77020f8af34bdf6
nickbloo <nickbloo2@gmail.com>
2022-12-23 01:06:42 -0600

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-blue-600">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
