85bd90350c79a47335699e5f3c2df81abf50c386
nickbloo <nickbloo2@gmail.com>
2023-01-14 18:59:04 -0600

    responsive styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,12 @@ const About = () => {
   
   return (
     <div className='flex flex-wrap h-screen flex-row justify-center items-center pl-3 pt-14 text-white text-left' id="about">
-      <img className='rounded-full h-3/6 md:h-5/6' src={me} alt="pic-of-me"/>
-      <div className='pl-3'>
-        <div className='flex text-1xl md:text-4xl font-bold'>I am a software developer from Oklahoma.</div> 
-        <div className='flex text-1xl md:text-4xl font-bold'>I enjoy building things where I can utilize my creative energy.</div>
-        <div className='flex text-1xl md:text-4xl font-bold'>I have Front End and Back End experience and</div>
-        <div className='flex text-1xl md:text-4xl font-bold'>I am actively applying for full-time positions :)</div>
+      <img className='rounded-full h-2/6 md:h-4/6' src={me} alt="pic-of-me"/>
+      <div className='pl-3 pb-20 md:pb-0'>
+        <div className='text-1xl md:text-4xl font-bold'>I am a software developer from Oklahoma.</div> 
+        <div className='text-1xl md:text-4xl font-bold'>I enjoy building creative things.</div>
+        <div className='text-1xl md:text-4xl font-bold'>I have Front End and Back End experience and</div>
+        <div className='text-1xl md:text-4xl font-bold'>I am actively applying for full-time positions :)</div>
       </div>
     </div>
   )
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,17 +5,16 @@ import tictactoe from '../assets/tictactoe.png';
 import { FaReact, FaNode } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 
-
 const Portfolio = () => {
   
   return (
-    <div className='flex flex-wrap flex-col h-screen justify-center items-center pl-3 pt-14 text-white' id="portfolio">
-      <h1 className='text-7xl font-semibold'>Portfolio</h1>
+    <div className='flex mb-3 md:mb-0 flex-wrap md:flex-col h-screen justify-center items-center pl-3 pt-14 text-white' id="portfolio">
+      <h1 className='md:text-7xl text-5xl font-semibold'>Portfolio</h1>
 
-      <div className='flex flex-row gap-x-48 mt-2'>
-        <div className='rounded p-6 shadow-2xl'>
-          <h2 className='text-4xl font-semibold'>Threadline</h2>
-          <img src={threadline} alt="threadline" height='400' width='400'/>
+      <div className='flex md:flex-row flex-col gap-x-48 mt-2'>
+        <div className='rounded md:p-6 pl-1 md:shadow-2xl'>
+          <h2 className='text-2xl md:text-4xl font-semibold'>Threadline</h2>
+          <img className='w-36 md:w-96' src={threadline} alt="threadline"/>
           <p>Full Stack e-commerce fashion app</p>
           <span className='flex flex-row'>
             <FaReact className='p-1 text-cyan-400' size={50}/>
@@ -24,35 +23,35 @@ const Portfolio = () => {
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png" alt="postgres" height={50} width={50}/>
           </span>
           <a href="https://github.com/orgs/Capstone-Group-3/repositories" target="_blank">
-            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>View on github</button>
+            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold md:py-2 py-1 md:px-4 px-2 text-sm border border-gray-400 rounded shadow'>View on github</button>
           </a>
         </div>
 
-        <div className='rounded p-6 shadow-2xl'>
-          <h2 className='text-4xl font-semibold'>Bible App</h2>
-          <img src={bibleapp} alt="bibleapp" height='400' width='400'/>
-          <p>Generates a daily Bible verse, prayer and good deed for users</p>
+        <div className='rounded md:p-6 pl-1 pt-1 md:shadow-2xl'>
+          <h2 className='text-2xl md:text-4xl font-semibold'>Bible App</h2>
+          <img className='w-36 md:w-96' src={bibleapp} alt="bibleapp"/>
+          <p>Generates a daily Bible verse, prayer and good deed</p>
           <span className='flex flex-row'>
             <FaReact className='p-1 text-cyan-400' size={50}/>
             <img src="https://vitejs.dev/logo-with-shadow.png" alt="vite" height={50} width={50}/>
             <img src="https://avatars.githubusercontent.com/u/22247014?s=280&v=4" alt="yarn" height={50} width={50}/>
           </span>
           <a href="https://github.com/Warriors-of-Friendship/bible-app" target="_blank">
-            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>View on github</button>
+            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold md:py-2 py-1 md:px-4 px-2 text-sm border border-gray-400 rounded shadow'>View on github</button>
           </a>
         </div>
 
-        <div className='rounded p-6 shadow-2xl'>
-          <h2 className='text-4xl font-semibold'>Tic-Tac-Toe</h2>
-          <img src={tictactoe} alt="tictactoe" height='400' width='400'/>
-          <p>Tic-Tac-Toe game app built with vanilla JavaScript</p>
+        <div className='rounded md:p-6 pl-1 pt-1 md:shadow-2xl'>
+          <h2 className='text-2xl md:text-4xl font-semibold'>Tic-Tac-Toe</h2>
+          <img className='w-36 md:w-96' src={tictactoe} alt="tictactoe"/>
+          <p>Tic-Tac-Toe app built with vanilla JavaScript</p>
           <span className='flex flex-row'>
             <img src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png" alt="javascript" height={30} width={50} className='pt-5 pr-1'/>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png" alt="css" height={50} width={50}/>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png" alt="html" height={50} width={50}/>
           </span>
           <a href="https://github.com/nickbloo/Project-3-Arcade-Game" target="_blank">
-            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>View on github</button>
+            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold md:p-6 pl-1 pt-1 md:shadow-2xl border border-gray-400 rounded shadow'>View on github</button>
           </a>
         </div>
 
